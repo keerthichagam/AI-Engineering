ba896d45ac618c77991a789dde028f378ce2d209
Sai Keerthi <keerthichagam1.@gmail.com>
2026-08-21 06:41:15 +0530

    Add Two Pointer

--- dsa.py
+++ dsa.py
@@ -69,3 +69,17 @@ while left<=right:
         right=mid-1
 else:
     print("Element not found")
+#Two pointer
+numbers = [2, 3, 4, 7, 11, 15]
+target = 9
+left=0
+right=len(numbers)-1
+while left<right:
+    current_sum=numbers[left]+numbers[right]
+    if current_sum==target:
+        print("Pair found",numbers[left],numbers[right])
+        break
+    elif current_sum<target:
+        left+=1
+    else:
+        right-=1
